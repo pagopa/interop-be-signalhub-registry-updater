1dbf741767dad97a9cba099348c8415d2728b502
francarl77 <97600561+francarl77@users.noreply.github.com>
2023-10-20 18:45:31 +0200

    update test

--- src/test/java/it/pagopa/interop/signalhub/updater/UpdaterApplicationTests.java
+++ src/test/java/it/pagopa/interop/signalhub/updater/UpdaterApplicationTests.java
@@ -2,7 +2,6 @@ package it.pagopa.interop.signalhub.updater;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
