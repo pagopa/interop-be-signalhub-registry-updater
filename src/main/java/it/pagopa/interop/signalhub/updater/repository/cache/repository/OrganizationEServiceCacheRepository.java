3d6d4aac5293dacc9eee8de0be65fcbb9160199c
cristopher.moccia <cristopher.moccia@accenture.com>
2023-10-20 12:41:11 +0200

    some fixes

--- src/main/java/it/pagopa/interop/signalhub/updater/repository/cache/repository/OrganizationEServiceCacheRepository.java
+++ src/main/java/it/pagopa/interop/signalhub/updater/repository/cache/repository/OrganizationEServiceCacheRepository.java
@@ -1,13 +1,11 @@
 package it.pagopa.interop.signalhub.updater.repository.cache.repository;
 
 
-import it.pagopa.interop.signalhub.updater.repository.cache.model.ConsumerEServiceCache;
 import it.pagopa.interop.signalhub.updater.repository.cache.model.OrganizationEServiceCache;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
