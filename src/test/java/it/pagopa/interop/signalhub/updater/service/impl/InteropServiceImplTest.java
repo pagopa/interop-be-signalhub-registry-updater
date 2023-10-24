468dcf0089ee18adb6b87ef2659168049bf33034
DIR\noemi.luongo <noemi.luongo@accenture.com>
2023-10-24 18:01:22 +0200

    add jUNIT SERVICE TEST

---
+++ src/test/java/it/pagopa/interop/signalhub/updater/service/impl/InteropServiceImplTest.java
@@ -0,0 +1,106 @@
+package it.pagopa.interop.signalhub.updater.service.impl;
+
+import it.pagopa.interop.signalhub.updater.exception.PDNDNoEventsException;
+import it.pagopa.interop.signalhub.updater.externalclient.InteroperabilityClient;
+import it.pagopa.interop.signalhub.updater.generated.openapi.client.interop.model.v1.Agreement;
+import it.pagopa.interop.signalhub.updater.generated.openapi.client.interop.model.v1.EService;
+import it.pagopa.interop.signalhub.updater.generated.openapi.client.interop.model.v1.Event;
+import it.pagopa.interop.signalhub.updater.generated.openapi.client.interop.model.v1.Events;
+import it.pagopa.interop.signalhub.updater.mapper.ConsumerEServiceMapper;
+import it.pagopa.interop.signalhub.updater.mapper.OrganizationEServiceMapper;
+import it.pagopa.interop.signalhub.updater.model.ConsumerEServiceDto;
+import it.pagopa.interop.signalhub.updater.model.EventsDto;
+import it.pagopa.interop.signalhub.updater.model.OrganizationEServiceDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static it.pagopa.interop.signalhub.updater.utility.Const.AGREEMENT_EVENT;
+import static it.pagopa.interop.signalhub.updater.utility.Const.ESERVICE_EVENT;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class InteropServiceImplTest {
+
+    @InjectMocks
+    private InteropServiceImpl interopService;
+
+    @Mock
+    private InteroperabilityClient client;
+    @Mock
+    private ConsumerEServiceMapper mapperConsumer;
+    @Mock
+    private OrganizationEServiceMapper mapperOrganization;
+
+    @Test
+    void whenCallGetAgreementsAndEServicesAndEventIsNullOrEmpty() {
+        Mockito.when(client.getEventsFromId(Mockito.any())).thenReturn(null);
+        Long lastEventId= 1l;
+        PDNDNoEventsException thrownIsNull = assertThrows(
+                PDNDNoEventsException.class,
+                () -> {
+                    interopService.getAgreementsAndEServices(1l);
+                }
+        );
+        assertEquals("No events from last event id ".concat(lastEventId.toString()), thrownIsNull.getMessage());
+
+        Mockito.when(client.getEventsFromId(Mockito.any())).thenReturn(new Events());
+
+        PDNDNoEventsException thrownIsEmpty = assertThrows(
+                PDNDNoEventsException.class,
+                () -> {
+                    interopService.getAgreementsAndEServices(1l);
+                }
+        );
+        assertEquals("No events from last event id ".concat(lastEventId.toString()), thrownIsEmpty.getMessage());
+
+    }
+
+    @Test
+    void getAgreementsAndEServices() {
+        Event event= new Event();
+        event.setEventId(1l);
+        event.setEventType("test");
+        event.setObjectId(new HashMap<>());
+        event.setObjectType(ESERVICE_EVENT);
+
+        List<Event> eventList= new ArrayList<>();
+        eventList.add(event);
+
+        Events events= new Events();
+        events.setEvents(eventList);
+
+        Mockito.when(client.getEventsFromId(Mockito.any())).thenReturn(events);
+        assertNotNull(interopService.getAgreementsAndEServices(1l));
+
+
+        event.setObjectType(AGREEMENT_EVENT);
+        Mockito.when(client.getEventsFromId(Mockito.any())).thenReturn(events);
+        assertNotNull(interopService.getAgreementsAndEServices(1l));
+
+    }
+
+    @Test
+    void getConsumerEService() {
+        Mockito.when(client.getAgreement(Mockito.any())).thenReturn(new Agreement());
+        Mockito.when(mapperConsumer.toConsumerEServiceDtoFromAgreement(Mockito.any(), Mockito.any())).thenReturn(new ConsumerEServiceDto());
+
+        assertNotNull(interopService.getConsumerEService("123", 1l));
+
+    }
+
+    @Test
+    void getEService() {
+        Mockito.when(client.getEService(Mockito.any())).thenReturn(new EService());
+        Mockito.when(mapperOrganization.fromEServiceToOrganizationEServiceDto(Mockito.any(), Mockito.any())).thenReturn(new OrganizationEServiceDto());
+        assertNotNull(interopService.getEService("123", 1l));
+    }
+}
---
+++ src/test/java/it/pagopa/interop/signalhub/updater/service/impl/TracingBatchServiceImplTest.java
@@ -0,0 +1,121 @@
+package it.pagopa.interop.signalhub.updater.service.impl;
+
+import it.pagopa.interop.signalhub.updater.config.RegistryUpdaterProps;
+import it.pagopa.interop.signalhub.updater.entity.TracingBatchEntity;
+import it.pagopa.interop.signalhub.updater.exception.PDNDBatchAlreadyExistException;
+import it.pagopa.interop.signalhub.updater.exception.PDNDEntityNotFound;
+import it.pagopa.interop.signalhub.updater.exception.PDNDNoEventsException;
+import it.pagopa.interop.signalhub.updater.mapper.TracingBatchMapper;
+import it.pagopa.interop.signalhub.updater.model.TracingBatchDto;
+import it.pagopa.interop.signalhub.updater.model.TracingBatchStateEnum;
+import it.pagopa.interop.signalhub.updater.repository.TracingBatchRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static it.pagopa.interop.signalhub.updater.model.TracingBatchStateEnum.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class TracingBatchServiceImplTest {
+
+    @InjectMocks
+    private TracingBatchServiceImpl tracingBatchService;
+
+    @Mock
+    private TracingBatchRepository repository;
+    @Mock
+    private TracingBatchMapper mapper;
+    @Mock
+    private RegistryUpdaterProps props;
+
+
+    @Test
+    void checkAndCreateTracingBatchButBatchAlreadyInRunning() {
+        Mockito.when(repository.findByStateProgressAndLastEventIdMax(Mockito.any())).thenReturn(Optional.of(new TracingBatchEntity()));
+
+        PDNDBatchAlreadyExistException thrown = assertThrows(
+                PDNDBatchAlreadyExistException.class,
+                () -> {
+                    tracingBatchService.checkAndCreateTracingBatch();
+                }
+        );
+        assertEquals("Batch already in running", thrown.getMessage());
+    }
+
+    @Test
+    void checkAndCreateTracingBatch() {
+        //lastEventId==0L
+        Mockito.when(repository.findByStateProgressAndLastEventIdMax(Mockito.any())).thenReturn(Optional.empty());
+        Mockito.when(repository.findByStateAndLastEventIdMax()).thenReturn(new ArrayList<>());
+        Mockito.when(repository.saveAndFlush(Mockito.any())).thenReturn(new TracingBatchEntity());
+        Mockito.when(mapper.toDto(Mockito.any())).thenReturn(new TracingBatchDto());
+        assertEquals(tracingBatchService.checkAndCreateTracingBatch().getLastEventId(), 0L);
+
+
+        //lastEventId==tracingBatchEntity.getLastEventId()
+        TracingBatchEntity tracingBatchEntity= new TracingBatchEntity();
+        tracingBatchEntity.setState(ENDED.name());
+        tracingBatchEntity.setLastEventId(1l);
+        List<TracingBatchEntity> list= new ArrayList<>();
+        list.add(tracingBatchEntity);
+        Mockito.when(repository.findByStateAndLastEventIdMax()).thenReturn(list);
+        assertEquals(tracingBatchService.checkAndCreateTracingBatch().getLastEventId(), tracingBatchEntity.getLastEventId());
+
+        //lastEventId==tracingBatchEntity.getLastEventId()+1
+        tracingBatchEntity.setState("test");
+        list= new ArrayList<>();
+        list.add(tracingBatchEntity);
+        Mockito.when(repository.findByStateAndLastEventIdMax()).thenReturn(list);
+        Mockito.when(props.getAttemptEvent()).thenReturn(0);
+        assertEquals(tracingBatchService.checkAndCreateTracingBatch().getLastEventId(), tracingBatchEntity.getLastEventId()+1);
+
+        //lastEventId==tracingBatchEntity.getLastEventId()
+        Mockito.when(props.getAttemptEvent()).thenReturn(3);
+        assertEquals(tracingBatchService.checkAndCreateTracingBatch().getLastEventId(), tracingBatchEntity.getLastEventId());
+
+    }
+
+    @Test
+    void countBatchInErrorWithLastEventId() {
+        Mockito.when(repository.findAllStateEndedWithErrorAndLastEventId(Mockito.any(), Mockito.any())).thenReturn(null);
+        assertEquals(tracingBatchService.countBatchInErrorWithLastEventId(1l), 0);
+
+        Mockito.when(repository.findAllStateEndedWithErrorAndLastEventId(Mockito.any(), Mockito.any())).thenReturn(new ArrayList<>());
+        assertEquals(tracingBatchService.countBatchInErrorWithLastEventId(1l), 0);
+
+    }
+
+    @Test
+    void terminateTracingBatchButBatchNotFound() {
+        Long batchId= 1l;
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        PDNDEntityNotFound thrown = assertThrows(
+                PDNDEntityNotFound.class,
+                () -> {
+                    tracingBatchService.terminateTracingBatch(batchId, IN_PROGRESS, 1l );
+                }
+        );
+        assertEquals("Batch entity not founded with ".concat(batchId.toString()).concat(" id"), thrown.getMessage());
+    }
+
+    @Test
+    void terminateTracingBatch() {
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.of(new TracingBatchEntity()));
+        Mockito.when(repository.saveAndFlush(Mockito.any())).thenReturn(new TracingBatchEntity());
+        Mockito.when(mapper.toDto(Mockito.any())).thenReturn(new TracingBatchDto());
+        assertNotNull(tracingBatchService.terminateTracingBatch(1l, ENDED, 1L ));
+
+    }
+
+
+}
