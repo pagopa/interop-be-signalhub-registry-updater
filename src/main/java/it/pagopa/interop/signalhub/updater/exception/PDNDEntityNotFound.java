cbd491729d7164c43eb28758fa7e93fcab62aeef
pasqualino.cristaudo <>
2023-11-08 16:28:05 +0100

    Removed a custom exception

--- src/main/java/it/pagopa/interop/signalhub/updater/exception/PDNDEntityNotFound.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.interop.signalhub.updater.exception;
-
-public class PDNDEntityNotFound extends RuntimeException {
-
-
-    public PDNDEntityNotFound(String message) {
-        super(message);
-    }
-}
