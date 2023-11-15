7c3ea1fb60b0bbfc428eb496e371780a6aa1913f
cristopher.moccia <cristopher.moccia@accenture.com>
2023-11-15 16:44:36 +0100

    fixed redis connection

--- src/main/java/it/pagopa/interop/signalhub/updater/config/RedisCacheConfig.java
+++ src/main/java/it/pagopa/interop/signalhub/updater/config/RedisCacheConfig.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -18,23 +18,9 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @Slf4j
 public class RedisCacheConfig {
 
-
-
-    @Bean
-    public JedisConnectionFactory connectionFactory() {
-        log.info("Redis configuration: [ hostname = " + "localHost" + " - port = " + "6379" + "]");
-
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        String redisHostname = "localhost";
-        int redisPort = 6379;
-        configuration.setHostName(redisHostname);
-        configuration.setPort(redisPort);
-        return new JedisConnectionFactory(configuration);
-    }
-
     @Bean
     @Qualifier("RedisTemplateConsumer")
-    RedisTemplate<String, ConsumerEServiceCache> redisTemplate(JedisConnectionFactory connectionFactory){
+    RedisTemplate<String, ConsumerEServiceCache> redisTemplate(RedisConnectionFactory connectionFactory){
         RedisTemplate<String,ConsumerEServiceCache> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(RedisSerializer.string());
@@ -46,7 +32,7 @@ public class RedisCacheConfig {
 
     @Bean
     @Qualifier("RedisTemplateOrganization")
-    RedisTemplate<String, OrganizationEServiceCache> redisTemplateOrganization(JedisConnectionFactory connectionFactory){
+    RedisTemplate<String, OrganizationEServiceCache> redisTemplateOrganization(RedisConnectionFactory connectionFactory){
         RedisTemplate<String, OrganizationEServiceCache> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(RedisSerializer.string());
